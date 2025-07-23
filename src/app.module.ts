322a2c79b81b460b7d99b74ac6d96338a65c9328
beatrizvzz-lab <lirabeatriz.001@gmail.com>
2025-07-23 18:40:53 -0300

    integrado ConfigService para uso de variáveis .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [MoviesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MoviesModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,1 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: 'http://localhost:8080',
-  });
-
-  const config = new DocumentBuilder()
-    .setTitle('Movies API')
-    .setDescription('API for listing and viewing movie details')
-    .setVersion('1.0')
-    .addTag('movies')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document); // Documentação acessível em http://localhost:3002/docs
-
-  await app.listen(process.env.PORT ?? 3002);
-}
-
-void bootstrap();
+import { NestFactory } from '@nestjs/core';import { AppModule } from './app.module';import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';import { ConfigService } from '@nestjs/config';async function bootstrap() {  const app = await NestFactory.create(AppModule);  const configService = app.get(ConfigService);  const URL_FRONT = configService.get<string>('URL_FRONT');  const PORT = configService.get<number>('PORT') || 3002;  app.enableCors({    origin: URL_FRONT,    credentials: true,  });  const config = new DocumentBuilder()    .setTitle('Movies API')    .setDescription('API for listing and viewing movie details')    .setVersion('1.0')    .addTag('movies')    .build();  const document = SwaggerModule.createDocument(app, config);  SwaggerModule.setup('docs', app, document); // Documentação acessível em http://localhost:3002/docs  await app.listen(PORT);}void bootstrap();
